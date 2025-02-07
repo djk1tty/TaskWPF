45aa462c04ccda6e0892d1937fbc3fe10e93f867
student <student@000c297a764f>
2025-02-07 18:40:07 +0300

    Добавьте файлы проекта.

---
+++ WPF_Task/PagesController/AuthorizePageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Task.PagesController
+{
+    public class AuthorizePageController
+    {
+        public User FindUserByLoginAndPassword(string login, string password)
+        {
+            if (login == "")
+            {
+                throw new Exception("Ошибка. Логин не заполнен.");
+            }
+
+            if (password == "")
+            {
+                throw new Exception("Ошибка. Пароль не заполнен.");
+            }
+
+            User user;
+            try
+            {
+                user = DbConnector.KondiEntities.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Ошибка соединения с БД");
+            }
+
+            if (user == null)
+            {
+                throw new Exception("Ошибка. Пользователь с такоей парой логин и пароль не найден.");
+            }
+
+            return user;
+        }
+    }
+}
